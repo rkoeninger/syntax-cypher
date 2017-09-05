3a077805e5e5661ff2c564731900609598a4efdf
Robert Koeninger <rkoeninger@att.net>
2017-09-04 21:54:40 -0400

    Added gulpfile and gulp-livescript as a dev dependency

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+    ls = require('gulp-livescript');
+
+// define tasks here
+gulp.task('default', function(){
+    console.log('test');
+    // run tasks here
+    // set up watch handlers here
+});
--- index.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-// define tasks here
-gulp.task('default', function(){
-  // run tasks here
-  // set up watch handlers here
-});
