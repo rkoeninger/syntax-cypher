c1b7136cad0373b74109816801b2069f49e32edf
Robert Koeninger <rkoeninger@att.net>
2017-09-08 11:31:41 -0400

    Re-arranged directory structure, now ls/main, ls/test, js/main, js/test

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,25 @@ var del = require("del"),
     gulp = require("gulp"),
     mocha = require("gulp-mocha"),
     gutil = require("gulp-util"),
-    ls = require("gulp-livescript");
+    ls = require("gulp-livescript"),
+
+    lsFiles = "./ls/**/*.ls",
+    jsRoot = "./js",
+    testFiles = jsRoot + "/test/**/*.js";
 
 gulp.task("default", ["test"]);
 
-gulp.task("build-src", () =>
-    gulp.src("./src/**/*.ls")
+gulp.task("build", () =>
+    gulp.src(lsFiles)
         .pipe(ls({bare: true}).on("error", gutil.log))
-        .pipe(gulp.dest("./js")));
-
-gulp.task("build-test", () =>
-    gulp.src("./test/**/*.ls")
-        .pipe(ls({bare: true}).on("error", gutil.log))
-        .pipe(gulp.dest("./testjs")));
-
-gulp.task("build", ["build-src", "build-test"]);
+        .pipe(gulp.dest(jsRoot)));
 
 gulp.task("test", ["build"], () =>
-    gulp.src("./testjs/**/*.js", {read: false})
+    gulp.src(testFiles, {read: false})
         .pipe(mocha({reporter: "spec"})));
 
-gulp.task("clean", () => del(["./js", "./testjs"]));
+gulp.task("clean", () => del([jsRoot]));
 
 gulp.task("watch", () => {
-    gulp.watch("./src/**/*.ls", ["build"]);
-    gulp.watch("./test/**/*.ls", ["build"]);
+    gulp.watch(lsFiles, ["build"]);
 });
