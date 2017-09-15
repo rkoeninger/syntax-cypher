5ad3b63df6b388a4460d89f239e2cc4cef85db61
Robert Koeninger <rkoeninger@att.net>
2017-09-14 23:57:53 -0400

    Enabled webpack

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,25 @@ var del = require("del"),
     mocha = require("gulp-mocha")({reporter: "spec"}),
     gutil = require("gulp-util"),
     lsc = require("gulp-livescript")({bare: true}).on("error", gutil.log),
+    webpack = require("webpack-stream")({config: {output: {filename: "bundle.js"}}}),
 
     dest = gulp.dest.bind(gulp),
     src = gulp.src.bind(gulp),
     task = gulp.task.bind(gulp),
     watch = gulp.watch.bind(gulp),
 
-    jsRoot = "./js",
     lsFiles = "./ls/**/*.ls",
-    testFiles = jsRoot + "/test/**/*.js";
+    jsRoot = "./js",
+    jsFiles = jsRoot + "/**/*.js",
+    jsMainFiles = jsRoot + "/main/**/*.js",
+    jsTestFiles = jsRoot + "/test/**/*.js";
 
 task("build", () => src(lsFiles).pipe(lsc).pipe(dest(jsRoot)));
 
 task("clean", () => del([jsRoot]));
 
-task("test", ["build"], () => src(testFiles, {read: false}).pipe(mocha));
+task("test", ["build"], () => src(jsTestFiles, {read: false}).pipe(mocha));
 
 task("watch", () => watch(lsFiles, ["test"]));
+
+task("webpack", ["build"], () => src(jsMainFiles).pipe(webpack).pipe(dest(jsRoot)));
