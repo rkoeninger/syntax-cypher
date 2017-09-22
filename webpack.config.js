3e63559257a665112b04a06c508faab0ef6bc0d2
Robert Koeninger <rkoeninger@att.net>
2017-09-22 16:24:29 -0400

    Moved katex (incl. fonts) and bulma css into bundle

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,12 @@ module.exports = {
         loaders: [{
             test: /.css$/,
             use: ["style-loader", "css-loader"]
-        }]
+        }, {
+            test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+            loader: 'file-loader?name=fonts/[name].[ext]'
+        }/*, {
+            test: /^http(s)?\;\/\//,
+            loader: 'url-loader'
+        }*/]
     }
 };
