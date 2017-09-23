639cc7420d641551ffe11bdc8fabbb4c8d04d3d0
Robert Koeninger <rkoeninger@att.net>
2017-09-23 14:11:19 -0400

    Replaced tabs with spaces

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     },
     resolve: {
         alias: {
-        	// vue alias is needed so we get the runtime+compiler version
-        	// of vue instead of the default runtime-only version
+            // vue alias is needed so we get the runtime+compiler version
+            // of vue instead of the default runtime-only version
             vue: "vue/dist/vue.js"
         }
     },
