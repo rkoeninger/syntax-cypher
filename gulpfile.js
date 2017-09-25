f9043d44b7cc0adc2f009ac28f46d4c874285387
Robert Koeninger <rkoeninger@att.net>
2017-09-25 10:54:46 -0400

    Replaces ls, js folders with src, build

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,18 @@ var del = require("del"),
     task = gulp.task.bind(gulp),
     watch = gulp.watch.bind(gulp),
 
-    lsFiles = "./ls/**/*.ls",
-    jsRoot = "./js",
-    jsFiles = jsRoot + "/**/*.js",
-    jsMainFiles = jsRoot + "/main/**/*.js",
-    jsTestFiles = jsRoot + "/test/**/*.js",
+    lsFiles = "./src/**/*.ls",
+    buildRoot = "./build",
+    jsFiles = buildRoot + "/**/*.js",
+    jsMainFiles = buildRoot + "/main/**/*.js",
+    jsTestFiles = buildRoot + "/test/**/*.js",
     distRoot = "./dist";
 
-task("build", () => src(lsFiles).pipe(lsc).pipe(dest(jsRoot)));
+task("build", () => src(lsFiles).pipe(lsc).pipe(dest(buildRoot)));
 
 task("bundle", ["build"], () => src(jsMainFiles).pipe(webpack).pipe(dest(distRoot)));
 
-task("clean", () => del([jsRoot, distRoot]));
+task("clean", () => del([buildRoot, distRoot]));
 
 task("test", ["build"], () => src(jsTestFiles, {read: false}).pipe(mocha));
 
